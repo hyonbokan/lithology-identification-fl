31f81f7bc58fddfcac8b201891811a24df2895b7
DNLab <mkkanhb@gmail.com>
2023-06-24 12:41:04 +0900

    adding script for preprocessing fl and cl0

---
+++ fl/preprocessing.py
@@ -0,0 +1,207 @@
+import pandas as pd
+import numpy as np
+
+# loading saved penalty matrix
+A = np.load('penalty_matrix.npy')
+
+
+def drop_columns(data, *args):    
+    columns = []
+    for _ in args:
+        columns.append(_)
+        
+    data = data.drop(columns, axis=1)
+        
+    return data
+
+
+def process(data):
+    cols = list(data.columns)
+    for _ in cols:  
+        data[_] = np.where(data[_] == np.inf, -999, data[_])
+        data[_] = np.where(data[_] == np.nan, -999, data[_])
+        data[_] = np.where(data[_] == -np.inf, -999, data[_])
+        
+    return data
+
+def fill_missing_values(df):    
+    cols = list(df.columns)
+    for _ in cols:
+
+        df[_] = np.where(df[_] == np.inf, -999, df[_])
+        df[_] = np.where(df[_] == np.nan, -999, df[_])
+        df[_] = np.where(df[_] == -np.inf, -999, df[_])
+        
+    return df
+
+
+#Paulo Bestagini's feature augmentation technique from SEG 2016 ML competition
+#Link : https://github.com/seg/2016-ml-contest/tree/master/ispl
+
+
+# Feature windows concatenation function
+def augment_features_window(X, N_neig):
+    
+    # Parameters
+    N_row = X.shape[0]
+    N_feat = X.shape[1]
+ 
+    # Zero padding
+    X = np.vstack((np.zeros((N_neig, N_feat)), X, (np.zeros((N_neig, N_feat)))))
+ 
+    # Loop over windows
+    X_aug = np.zeros((N_row, N_feat*(2*N_neig+1)))
+    for r in np.arange(N_row)+N_neig:
+        this_row = []
+        for c in np.arange(-N_neig,N_neig+1):
+            this_row = np.hstack((this_row, X[r+c]))
+        X_aug[r-N_neig] = this_row
+ 
+    return X_aug
+ 
+# Feature gradient computation function
+def augment_features_gradient(X, depth):
+    
+    # Compute features gradient
+    d_diff = np.diff(depth).reshape((-1, 1))
+    d_diff[d_diff==0] = 0.001
+    X_diff = np.diff(X, axis=0)
+    X_grad = X_diff / d_diff
+        
+    # Compensate for last missing value
+    X_grad = np.concatenate((X_grad, np.zeros((1, X_grad.shape[1]))))
+    
+    return X_grad
+ 
+# Feature augmentation function
+def augment_features(X, well, depth, N_neig=1):
+    
+    # Augment features
+    X_aug = np.zeros((X.shape[0], X.shape[1]*(N_neig*2+2)))
+    for w in np.unique(well):
+        w_idx = np.where(well == w)[0]
+        X_aug_win = augment_features_window(X[w_idx, :], N_neig)
+        X_aug_grad = augment_features_gradient(X[w_idx, :], depth[w_idx])
+        X_aug[w_idx, :] = np.concatenate((X_aug_win, X_aug_grad), axis=1)
+    
+    # Find padded rows
+    padded_rows = np.unique(np.where(X_aug[:, 0:7] == np.zeros((1, 7)))[0])
+    
+    return X_aug, padded_rows
+
+def score(y_true, y_pred):
+
+    '''
+    custom metric used for evaluation
+    args:
+      y_true: actual prediction
+      y_pred: predictions made
+    '''
+
+    S = 0.0
+    y_true = y_true.astype(int)
+    y_pred = y_pred.astype(int)
+    for i in range(0, y_true.shape[0]):
+        S -= A[y_true[i], y_pred[i]]
+    return S/y_true.shape[0]
+
+
+def preprocess(self, train, test):
+
+        '''
+        method to prepare datasets for training and predictions
+        accepts both the train and test dataframes as arguments
+
+        returns the prepared train, test datasets along with the
+        lithology labels and numbers which is needed for preparing
+        the submission file
+
+        '''
+
+        #concatenating both train and test datasets for easier and uniform processing
+
+        ntrain = train.shape[0]
+        ntest = test.shape[0]
+        target = train.FORCE_2020_LITHOFACIES_LITHOLOGY.copy()
+        df = pd.concat((train, test)).reset_index(drop=True)
+
+        #mapping the lithology labels to ordinal values for better modelling
+
+        lithology = train['FORCE_2020_LITHOFACIES_LITHOLOGY']
+ 
+        lithology_numbers = {30000: 0,
+                        65030: 1,
+                        65000: 2,
+                        80000: 3,
+                        74000: 4,
+                        70000: 5,
+                        70032: 6,
+                        88000: 7,
+                        86000: 8,
+                        99000: 9,
+                        90000: 10,
+                        93000: 11}
+        
+        lithology1 = lithology.map(lithology_numbers)
+
+        #implementing Bestagini's augmentation procedure
+
+        train_well = train.WELL.values
+        train_depth = train.DEPTH_MD.values
+        
+        test_well = test.WELL.values
+        test_depth = test.DEPTH_MD.values  
+        '''to be continued...
+        #this was done here for ease as the datasets would undergo some transformations
+        #that would make it uneasy to perform the augmentation technique'''
+
+        
+
+        print(f'shape of concatenated dataframe before dropping columns {df.shape}')
+
+        cols = ['FORCE_2020_LITHOFACIES_CONFIDENCE', 'SGR', 'DTS', 'RXO', 'ROPA'] #columns to be dropped
+        df = drop_columns(df, *cols)
+        print(f'shape of dataframe after dropping columns {df.shape}')
+        print(f'{cols} were dropped')
+
+        #Label encoding the GROUP, FORMATION and WELLS features as these improved the performance of the models on validations
+
+        df['GROUP_encoded'] = df['GROUP'].astype('category')
+        df['GROUP_encoded'] = df['GROUP_encoded'].cat.codes 
+        df['FORMATION_encoded'] = df['FORMATION'].astype('category')
+        df['FORMATION_encoded'] = df['FORMATION_encoded'].cat.codes
+        df['WELL_encoded'] = df['WELL'].astype('category')
+        df['WELL_encoded'] = df['WELL_encoded'].cat.codes
+        print(f'shape of dataframe after label encoding columns {df.shape}')
+
+
+        #FURTHER PREPRATION TO SPLIT DATAFRAME INTO TRAIN AND TEST DATASETS AFTER PREPRATION
+        print(f'Splitting concatenated dataframe into training and test datasets...')
+        df = df.drop(['WELL', 'GROUP', 'FORMATION'], axis=1)
+        print(df.shape)
+        
+        df = df.fillna(-999)
+        df = process(df)
+        data = df.copy()
+
+        print(f'dataframe columns: {data.columns}')
+        
+        train2 = data[:ntrain].copy()
+        train2.drop(['FORCE_2020_LITHOFACIES_LITHOLOGY'], axis=1, inplace=True)
+        
+        test2 = data[ntrain:(ntest+ntrain)].copy()
+        test2.drop(['FORCE_2020_LITHOFACIES_LITHOLOGY'], axis=1, inplace=True)
+        test2 = test2.reset_index(drop=True)
+
+        traindata = train2
+        testdata = test2
+
+        print(f'Shape of train and test datasets BEFORE augmentation {traindata.shape, testdata.shape}')
+ 
+        traindata1, padded_rows = augment_features(pd.DataFrame(traindata).values, train_well, train_depth)
+        testdata1, padded_rows = augment_features(pd.DataFrame(testdata).values, test_well, test_depth)
+        
+
+        print(f'Shape of train and test datasets AFTER augmentation {traindata1.shape, testdata1.shape}')
+    
+        return traindata1, testdata1, lithology1, lithology_numbers
