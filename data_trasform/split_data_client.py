f1a9a8cc69256c2850a9bb530ca91d1e88102158
DNLab <mkkanhb@gmail.com>
2023-08-26 20:17:17 +0900

    fl test - done

--- data_trasform/split_data_client.py
+++ data_trasform/split_data_client.py
@@ -1,15 +1,19 @@
 import os
 import pandas as pd
+import argparse
 
+parser = argparse.ArgumentParser(description="Split data into client-specific train and test CSV files.")
+parser.add_argument("--data_dir", required=True, help="Path to the data directory.")
+parser.add_argument("--save_dir", required=True, help="Path to the directory where client-specific files will be saved.")
+parser.add_argument("--num_clients", type=int, required=True, help="Number of clients.")
+args = parser.parse_args()
 
-pwd = '/media/Data-B/my_research/Geoscience_FL/data_well_log/'
-save_dir = f'{pwd}cl_data/'
+data_dir = args.data_dir
+save_dir = args.save_dir
+num_clients = args.num_clients
 
-num_clients = 2 # hardcode the number of clients
-
-
-train = pd.read_csv(pwd + 'train.csv', sep=';')
-test = pd.read_csv(pwd + 'test_with_lables.csv')
+train = pd.read_csv(os.path.join(data_dir, 'train.csv'), sep=';')
+test = pd.read_csv(os.path.join(data_dir, 'test_with_lables.csv'))
 
 unique_train_wells = train['WELL'].unique()
 unique_test_wells = test['WELL'].unique()
@@ -17,32 +21,22 @@ unique_test_wells = test['WELL'].unique()
 train_wells_per_client = len(unique_train_wells) // num_clients
 test_wells_per_client = len(unique_test_wells) // num_clients
 
-
 train_clients = []
 start_index = 0
 for i in range(num_clients):
     end_index = start_index + train_wells_per_client if i < num_clients - 1 else len(unique_train_wells)
-    
     client_train_wells = unique_train_wells[start_index:end_index]
-    
     client_train_df = train[train['WELL'].isin(client_train_wells)]
-    
     train_clients.append(client_train_df)
-    
     start_index = end_index
-
 
 test_clients = []
 start_index = 0
 for i in range(num_clients):
     end_index = start_index + test_wells_per_client if i < num_clients - 1 else len(unique_test_wells)
-    
     client_test_wells = unique_test_wells[start_index:end_index]
-    
     client_test_df = test[test['WELL'].isin(client_test_wells)]
-    
     test_clients.append(client_test_df)
-    
     start_index = end_index
 
 if not os.path.exists(save_dir):
--- data_trasform/split_train_val.py
+++ data_trasform/split_train_val.py
@@ -1,17 +1,25 @@
 import os
 import random
 import shutil
+import argparse
 
+parser = argparse.ArgumentParser(description="Split files into train, validation, and test sets.")
+parser.add_argument("--source_dir", required=True, help="Path to the source directory containing files.")
+parser.add_argument("--train_dir", required=True, help="Path to the train directory.")
+parser.add_argument("--val_dir", required=True, help="Path to the validation directory.")
+parser.add_argument("--test_dir", required=True, help="Path to the test directory.")
+parser.add_argument("--train_count", type=int, default=98, help="Number of files for the train set.")
+parser.add_argument("--val_count", type=int, default=10, help="Number of files for the validation set.")
+parser.add_argument("--test_count", type=int, default=10, help="Number of files for the test set.")
+args = parser.parse_args()
 
-train_count = 98
-val_count = 10
-test_count = 10
-
-source_directory = '/media/Data-B/my_research/Geoscience_FL/data_well_log/las_files_Lithostrat_data'
-
-train_dir = '/media/Data-B/my_research/Geoscience_FL/data_well_log/train/'
-val_dir = '/media/Data-B/my_research/Geoscience_FL/data_well_log/val/'
-test_dir = '/media/Data-B/my_research/Geoscience_FL/data_well_log/test/'
+source_directory = args.source_dir
+train_dir = args.train_dir
+val_dir = args.val_dir
+test_dir = args.test_dir
+train_count = args.train_count
+val_count = args.val_count
+test_count = args.test_count
 
 if not os.path.exists(train_dir):
     os.makedirs(train_dir)
@@ -19,13 +27,11 @@ if not os.path.exists(train_dir):
 else:
     print("Train directory already exists.")
 
-
 if not os.path.exists(val_dir):
     os.makedirs(val_dir)
     print("Validation directory created.")
 else:
     print("Validation directory already exists.")
-
 
 if not os.path.exists(test_dir):
     os.makedirs(test_dir)
@@ -33,16 +39,12 @@ if not os.path.exists(test_dir):
 else:
     print("Test directory already exists.")
 
-
 files = os.listdir(source_directory)
-
 random.shuffle(files)
-
 
 train_files = files[:train_count]
 val_files = files[train_count:train_count+val_count]
 test_files = files[train_count+val_count:train_count+val_count+test_count]
-
 
 for file_name in train_files:
     src = os.path.join(source_directory, file_name)
